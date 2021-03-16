b1e5a56295b5284c7e653bf29a30c1f677feb06a
Bo Stevenson <bostevenson1027@gmail.com>
2021-03-16 18:05:28 -0500

    added to search form component and imported bootstrap

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+function SearchForm(props) {
+    return(
+        <div className = "container">
+            <nav className= "navbar navbar-secondary bg-secondary">
+                <span className="navbar-brand mb-0 h1" id="heading"> Employee Directory</span>
+                <form className= "form-inline">
+                    <input
+                    className="form-control mr-sm-2" 
+                    type="search" 
+                    placeholder="Search Occupation" 
+                    aria-label="Search"
+                    name="occupation"
+                    value={props.occupation}
+                    onChange={props.handleInputChange}>
+                    
+                    </input>
+                </form>
+            </nav>
+        </div>
+    )
+}
+
+export default SearchForm;
